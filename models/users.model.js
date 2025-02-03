e50c2df20aa4f61a027107409527b416d1eb9ca0
hannahchapman79 <hannahchapman1999@aol.com>
2025-02-03 11:55:23 +0700

    Adds user schema and model for mongoose

---
+++ models/users.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+      },
+      eventsAttended: [{
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Event'
+    }],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = { User }
