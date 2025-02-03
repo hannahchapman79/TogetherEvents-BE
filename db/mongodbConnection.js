c3bb9e1b702eac8ca19919d14cbacb5901d5f679
hannahchapman79 <hannahchapman1999@aol.com>
2025-02-03 10:29:26 +0700

    Adds mongoDB connection file

---
+++ db/mongodbConnection.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const uri = process.env.DATABASE_URI;
+
+const connectMongoDB = async () => {
+  try {
+    await mongoose.connect(uri);
+    console.log('MongoDB connected successfully');
+  } catch (err) {
+    console.error('MongoDB connection error:', err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectMongoDB;
