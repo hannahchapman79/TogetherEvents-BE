f8f83474f07d93f8bb5270ba602a77d73158bcd2
hannahchapman79 <hannahchapman1999@aol.com>
2025-02-05 13:14:06 +0700

    Adds patch event by id endpoint to allow an admin to edit an event

--- middleware/adminAuth.js
+++ middleware/adminAuth.js
@@ -1,8 +1,8 @@
 function verifyAdmin(request, response, next) {
-    if (!request.user.isAdmin) { 
-      return response.status(403).json({ message: "Access denied: Admins only" });
-    }
-    next();
+  if (!request.user.isAdmin) {
+    return response.status(403).json({ message: "Access denied: Admins only" });
   }
+  next();
+}
 
 module.exports = verifyAdmin;
--- routes/events.routes.js
+++ routes/events.routes.js
@@ -3,13 +3,20 @@ const {
   getEvents,
   getEventById,
   postEvents,
+  updateEventById,
 } = require("../controllers/events.controller");
 const verifyToken = require("../middleware/jwtAuth");
-const verifyAdmin = require("../middleware/adminAuth")
+const verifyAdmin = require("../middleware/adminAuth");
 
 const eventsRouter = express.Router();
 
-eventsRouter.route("/").get(getEvents).post(verifyToken, verifyAdmin, postEvents);
-eventsRouter.route("/:id").get(getEventById);
+eventsRouter
+  .route("/")
+  .get(getEvents)
+  .post(verifyToken, verifyAdmin, postEvents);
+eventsRouter
+  .route("/:id")
+  .get(getEventById)
+  .patch(verifyToken, verifyAdmin, updateEventById);
 
 module.exports = eventsRouter;
