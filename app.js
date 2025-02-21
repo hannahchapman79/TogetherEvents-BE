da3c420903da023bed95bfc73ede68b55018ac7e
hannahchapman79 <hannahchapman1999@aol.com>
2025-02-21 15:39:11 +0000

    Update cors

--- app.js
+++ app.js
@@ -14,9 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production' 
-    ? 'http://localhost:4000' 
-    : 'http://localhost:3000',
+  origin: 'http://localhost:3000',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
