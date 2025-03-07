f1da98a9e2c59a1f2aab410729a58e2a35d9e667
hannahchapman79 <hannahchapman1999@aol.com>
2025-03-07 15:57:29 +0700

    Update isAdmin checker

--- middleware/adminAuth.js
+++ middleware/adminAuth.js
@@ -1,7 +1,13 @@
-function verifyAdmin(request, response, next) {
-  if (!request.user.isAdmin) {
+const { User } = require("../models/users.model");
+
+async function verifyAdmin(request, response, next) {
+
+  const user = await User.findOne({ email: request.user.email }); // Fetch user from DB
+
+  if (!user?.isAdmin) {
     return response.status(403).json({ message: "Access denied: Admins only" });
   }
+
   next();
 }
 
